bafda4ca60fd8c6fd3dd42bd53ad2e5e24b20a01
nknikkka <126584422+nknikkka@users.noreply.github.com>
2024-10-15 21:24:28 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,16 +2,21 @@ function filterArray(numbers, value) {
     const filteredNumbers = []; 
 
     for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) 
-            {filteredNumbers.push(numbers[i]);} }
+        if (numbers[i] > value) {
+            filteredNumbers.push(numbers[i]);
+        }
+    }
     return filteredNumbers;
 }
-
 const outputDiv = document.getElementById('output');
 const results = [
     filterArray([1, 2, 3, 4, 5], 3),
     filterArray([1, 2, 3, 4, 5], 4),
     filterArray([1, 2, 3, 4, 5], 5),
     filterArray([12, 24, 8, 41, 76], 38),
-    filterArray([12, 24, 8, 41, 76], 20)];
-outputDiv.innerHTML = results.map(result => JSON.stringify(result)).join('<br>');
+    filterArray([12, 24, 8, 41, 76], 20)
+];
+
+results.forEach((result, index) => {
+    outputDiv.innerHTML += `<p>Результат ${index + 1}: [${result.join(", ")}]</p>`;
+});
